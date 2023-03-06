6aa6e5b1644250c413a30fad7a725947fdc418bf
Alexander <57245999+alexanderapp07@users.noreply.github.com>
2023-03-05 19:16:34 -0600

    Configuración de la plantilla

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,43 @@
 <x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
+    <div class="container-fluid">
+        <div class="row text-center">
+            <div class="col"></div>
 
-        <x-validation-errors class="mb-4" />
-
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
-                {{ session('status') }}
+            <div class="col-5">
+                <x-authentication-card>
+                    <x-slot name="logo">
+                        <x-authentication-card-logo />
+                    </x-slot>
+            
+                    <x-validation-errors/>
+            
+                    <div class="card" style="border-radius: 3em";>
+                        <div class="card-body">
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+                    
+                                <div class="form-group mb-4">
+                                    <x-label for="email" value="Correo Electrónico" class="label-form fw-bold mb-3 h3" />
+                                    <x-input class="form-control" id="email" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                                </div>
+                    
+                                <div class="form-group mb-4">
+                                    <x-label for="password" value="Contraseña" class="label-form fw-bold mb-3 h3" />
+                                    <x-input id="password" class="form-control" type="password" name="password" required autocomplete="current-password" />
+                                </div>
+                    
+                                <div class="form-group">                    
+                                    <x-button class="btn btn-primary">
+                                        {{ __('Iniciar sesión') }}
+                                    </x-button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </x-authentication-card>
             </div>
-        @endif
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <div>
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            </div>
-
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-authentication-card>
+            <div class="col"></div>
+        </div>
+    </div>
 </x-guest-layout>
