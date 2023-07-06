7c3e0cb9ac97d05653953cdf24ed2085dca77480
alexander <ac18107@ues.edu.sv>
2023-07-06 15:22:01 -0600

    update

--- database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
+++ database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->id();
+            //$table->id();
             $table->text('two_factor_secret')
                     ->after('password')
                     ->nullable();
